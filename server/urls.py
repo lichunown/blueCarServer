133d2b967ae7444ad98d78c66ebb53a273ac389d
lcy <lichunyang@stu.xidian.edu.cn>
2017-03-11 17:04:30 +0800

    add index

--- server/urls.py
+++ server/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^user/', include('m_user.urls')),
     url(r'^position/', include('position.urls')),
+    url(r'^$', views.index,name='index'),
 ]
