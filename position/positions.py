12b7110839fc7be777076e3e34005fc7992b13b9
lcy <lichunyang@stu.xidian.edu.cn>
2017-03-15 23:15:31 +0800

    add token check & time check

--- position/positions.py
+++ position/positions.py
@@ -1,22 +1,37 @@
 #encoding:utf-8
+from m_user.token import userToken
+import time
+
 class Position(object):# Latitude and longitude
     def __init__(self,Latitude=.0,longitude=.0):
         self.lat = Latitude
         self.lon = longitude
+        self.time = time.time()
 
 class ObjectPositions(object):
     def __init__(self):
         self._position = {}
-    def addPosition(self,token,lat,lon):
-        self._position[token] = Position(lat,lon)
+        self._CHECKTIMELIMIT = 5
+        self._checktime = time.time()
     def changePosition(self,token,lat,lon):
-        self._position[token] = Position(lat,lon)
-    def removePosition(token):
+        if userToken.getUser(token):
+            self._position[token] = Position(lat,lon)
+        else:
+            self.removePosition(token)
+    def removePosition(self,token):
         try:
+            print 'remove ' + token
             return self._position.pop(token)
         except KeyError:
             return None
+    def check(self):
+        tempposition = self._position.copy()
+        for token in tempposition:
+            if time.time() - tempposition[token].time >= self._CHECKTIMELIMIT:
+                self.removePosition(token)
     def getPositions(self):
+        if time.time()-self._checktime>=self._CHECKTIMELIMIT:
+            self.check()
         result = [(0,0),]*len(self._position)
         i = 0
         for s_token in self._position:
